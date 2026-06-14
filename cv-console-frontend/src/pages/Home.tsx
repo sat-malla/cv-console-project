1fa5caada11b665c318fd004720ef87e05dc6710
Sathvik Malla <sathvikmalla17@berkeley.edu>
2026-06-14 17:57:41 -0400

    directory/codebase reset

--- cv-console-frontend/src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-import VideoPanel from "../components/VideoPanel"
-
-export default function Home() {
-  return (
-    <div style={{ width: "640px", height: "480px" }}>
-      <VideoPanel url="ws://127.0.0.1:8000/video" />
-    </div>
-  )
-}
